2745d78dea392696667f79bc469cd21d3615800f
gopikrishnabandi <54024626+gopikrishnabandi@users.noreply.github.com>
2021-05-23 15:05:50 -0400

    commit
    commit

--- FastScrapper.py
+++ FastScrapper.py
@@ -11,42 +11,42 @@ import numpy as np
 import pandas as pd
 
 
-# capab=DesiredCapabilities.CHROME
-# capab["pageLoadStrategy"] = "eager"
-# chrome_options = Options()
-#
-# no=int(input("Enter no of stocks:"))
-# stocks=[]
-# for i in range(no):
-#     stock=input("Enter Stock Symbol")
-#     stocks.append(stock)
-#
-# driver=Chrome("C:\\Users\\gopib\\Downloads\\chromedriver.exe", desired_capabilities=capab,
-#                         options=chrome_options)
-# url="https://www.nasdaq.com/market-activity/quotes/historical"
-#
-# for stock in stocks:
-#     driver.get(url)
-#     driver.maximize_window()
-#     time.sleep(3)
-#     driver.find_element_by_id("find-symbol-input-dark").send_keys(stock,Keys.ENTER)
-#     if "No Results Found" in driver.page_source:
-#         print("Enter Correct Stock Symbol")
-#         quit()
-#     else:
-#         time.sleep(5)
-#         try:
-#             driver.find_element_by_id("_evidon-decline-button").click()
-#         except:
-#             # when doing multiple stocks cookie decline does not appear the second time, hence putting a try catch
-#             pass
-#         driver.find_element_by_xpath(
-#             "/html/body/div[2]/div/main/div[2]/div[4]/div/section/div/div[3]/ul/li[2]/a/span").click()
-#         time.sleep(5)
-#         driver.find_element_by_xpath("/html/body/div[2]/div/main/div[2]/div[4]/div[3]/div/div[1]/div/div[1]/div[3]/div/div/div/button[6]").click()
-#         time.sleep(2)
-#         driver.find_element_by_xpath(
-#             "/html/body/div[2]/div/main/div[2]/div[4]/div[3]/div/div[1]/div/div[1]/div[3]/button").click()
+capab=DesiredCapabilities.CHROME
+capab["pageLoadStrategy"] = "eager"
+chrome_options = Options()
+
+no=int(input("Enter no of stocks:"))
+stocks=[]
+for i in range(no):
+    stock=input("Enter Stock Symbol")
+    stocks.append(stock)
+
+driver=Chrome("C:\\Users\\gopib\\Downloads\\chromedriver.exe", desired_capabilities=capab,
+                        options=chrome_options)
+url="https://www.nasdaq.com/market-activity/quotes/historical"
+
+for stock in stocks:
+    driver.get(url)
+    driver.maximize_window()
+    time.sleep(3)
+    driver.find_element_by_id("find-symbol-input-dark").send_keys(stock,Keys.ENTER)
+    if "No Results Found" in driver.page_source:
+        print("Enter Correct Stock Symbol")
+        quit()
+    else:
+        time.sleep(5)
+        try:
+            driver.find_element_by_id("_evidon-decline-button").click()
+        except:
+            # when doing multiple stocks cookie decline does not appear the second time, hence putting a try catch
+            pass
+        driver.find_element_by_xpath(
+            "/html/body/div[2]/div/main/div[2]/div[4]/div/section/div/div[3]/ul/li[2]/a/span").click()
+        time.sleep(5)
+        driver.find_element_by_xpath("/html/body/div[2]/div/main/div[2]/div[4]/div[3]/div/div[1]/div/div[1]/div[3]/div/div/div/button[6]").click()
+        time.sleep(2)
+        driver.find_element_by_xpath(
+            "/html/body/div[2]/div/main/div[2]/div[4]/div[3]/div/div[1]/div/div[1]/div[3]/button").click()
 
 path="C:\\Users\\gopib\\Downloads\\Stocks"
 
--- StockAnalysisAndPlotting.py
+++ StockAnalysisAndPlotting.py
@@ -17,6 +17,7 @@ class StockAnalysis:
         # print(path)
         # csv_files=glob.glob(os.path.join(path,"*.csv"))
         csv_files = glob.glob("*.csv")
+
         df_dict={}
         for file in csv_files:
             stock_name=file.replace('.csv','').upper()
@@ -41,10 +42,8 @@ class StockAnalysis:
             df_dict[stock_name]['Close'].plot(figsize=(15,10))
             plt.legend(df_dict.keys())
 
-
         plt.savefig('Closing Prices')
         plt.close()
-
 
     def close_vs_moving_average(self):
         csv_files = glob.glob("*.csv")
@@ -74,8 +73,7 @@ class StockAnalysis:
             plt.savefig(filename)
             plt.close()
 
-    def daily_returns(self):
-
+    def daily_returns_combined(self):
         # daily_rets = df.copy()
         # print(df[1:])
         # print(df[:-1])
@@ -105,29 +103,140 @@ class StockAnalysis:
             # sns.distplot(df_dict['daily_return'].dropna(), bins = 100, color = 'red') #
             plt.xlabel('Date')
             plt.ylabel('Daily Returns')
-            plt.title('Daily Returns for stocks')
+            plt.title('Daily Returns for stocks combined plot')
 
         plt.legend(df_dict.keys())
-        plt.savefig('Daily Returns of stocks')
+        plt.savefig('Daily Returns of stocks combined')
+        plt.close()
 
-    def correlation(self):
+    def daily_returns_individual(self):
+        csv_files=glob.glob("*.csv")
+        for file in csv_files:
+            stock_name=file.replace(".csv","").upper()
+            df=pd.read_csv(file)
+            df['Date']=df['Date'].astype('datetime64[ns]')
+            df=df.set_index('Date')
+            df=df.rename(columns={'Close/Last':'Close'})
+            df['Close'] = df['Close'].astype('float')
+            df['Volume'] = df['Volume'].astype('int64')
+            df['Open'] = df['Open'].astype('float')
+            df['High'] = df['High'].astype('float')
+            df['Low'] = df['Low'].astype('float')
+            df_daily=df.copy()
+            df_daily[1:]=(df_daily[1:].values/df_daily[:-1].values)-1
+            df_daily=df_daily.rename(columns={'Close':'Daily Returns'})
+            df_daily=df_daily[1:]
+            plt.figure(figsize=(20,10))
+            plt.title('Daily Returns for '+stock_name)
+            plt.xlabel('Date')
+            plt.ylabel('Daily Returns')
+            df_daily['Daily Returns'].plot()
+            # for legend always surround by square brackets to print full name, sles it will show only first letter in legend
+            plt.legend([stock_name])
+            plt.savefig('Daily Returns of '+stock_name)
+            plt.close()
+
+    def correlation_close(self):
         csv_files = glob.glob("*.csv")
         df1 = pd.DataFrame()
+        # df2=pd.DataFrame()
+        #creating a string to get stock names for our plot title
+        stocks=''
         for file in csv_files:
             stock_name = file.replace('.csv', '').upper()
             df = pd.read_csv(file)
             df = df.drop(columns={'Volume', 'Open', 'High', 'Low'})
-            df = df.rename(columns={'Close/Last': 'Close'})
+            df = df.rename(columns={'Close/Last': 'Close'+"_"+stock_name})
             df['Date'] = df['Date'].astype('datetime64[ns]')
             df = df.set_index('Date')
             df1=df1.join(df,how="outer")
-            df2=df1.merge(df,how="outer",left_index=True,right_index=True)
+            # we can also use merge, merge gives more options than a join , we can join on columns too with merge unlike join which is only on index
+            # df2=df2.merge(df,how="outer",left_index=True,right_index=True)
+            stocks=stocks+stock_name+","
 
-        print(df1)
-        print(df2)
-        # corr = (df1.dropna()).corr()
-        # sns.heatmap(corr, annot=True)
-        # plt.show()
+        # removing the last trailing comma
+        stocks=stocks[:-1]
+        corr = df1.corr()
+        plt.figure(figsize=(20, 10))
+        sns.heatmap(corr, annot=True)
+        plt.title('Correlation of closing prices between stocks '+stocks)
+        plt.savefig('CorrelationOfStocksClose')
+        plt.close()
+
+
+    def correlation_daily_returns(self):
+        csv_files = glob.glob("*.csv")
+        df1 = pd.DataFrame()
+        # df2=pd.DataFrame()
+        #creating a string to get stock names for our plot title
+        stocks=''
+        for file in csv_files:
+            stock_name = file.replace('.csv', '').upper()
+            df = pd.read_csv(file)
+            df = df.drop(columns={'Volume', 'Open', 'High', 'Low'})
+            df = df.rename(columns={'Close/Last':'Close'})
+            df['Date'] = df['Date'].astype('datetime64[ns]')
+            df = df.set_index('Date')
+            df['Close']=df['Close'].astype(float)
+            df['Daily Returns'+stock_name]=df['Close'].pct_change()
+            df=df.drop(columns={'Close'})
+            df1=df1.join(df,how="outer")
+            # we can also use merge, merge gives more options than a join , we can join on columns too with merge unlike join which is only on index
+            # df2=df2.merge(df,how="outer",left_index=True,right_index=True)
+            stocks=stocks+stock_name+","
+
+        # removing the last trailing comma
+        stocks=stocks[:-1]
+        corr = df1.corr()
+        plt.figure(figsize=(20,10))
+        sns.heatmap(corr, annot=True)
+        plt.title('Correlation of daily returns between stocks '+stocks)
+        plt.savefig('CorrelationOfStocksDailyReturns')
+        plt.close()
+
+    def risk_estimation(self):
+        csv_files = glob.glob("*.csv")
+        df_dict={}
+        i=1
+        # creating a string to get stock names for our plot title
+        for file in csv_files:
+            stock_name = file.replace('.csv', '').upper()
+            df_dict[stock_name] = pd.read_csv(file)
+            df_dict[stock_name]['Date'] = df_dict[stock_name]['Date'].astype('datetime64[ns]')
+            df_dict[stock_name] = df_dict[stock_name].rename(columns={"Close/Last": "Close"})
+            df_dict[stock_name]['Close'] = df_dict[stock_name]['Close'].astype('float')
+            df_dict[stock_name]['Volume'] = df_dict[stock_name]['Volume'].astype('int64')
+            df_dict[stock_name]['Open'] = df_dict[stock_name]['Open'].astype('float')
+            df_dict[stock_name]['High'] = df_dict[stock_name]['High'].astype('float')
+            df_dict[stock_name]['Low'] = df_dict[stock_name]['Low'].astype('float')
+            df_dict[stock_name] = df_dict[stock_name].set_index('Date')
+            df_dict[stock_name]['avg_daily_return'] = df_dict[stock_name]['Close'].pct_change().mean()
+            df_dict[stock_name]['daily_risk'] = df_dict[stock_name]['Close'].pct_change().std()
+            if i==1:
+                plt.figure(figsize=(20,10))
+                i+=1
+            else:
+                pass
+            plt.xlabel('Daily Average Expected Return')
+            plt.ylabel('Daily Risk')
+            # s is the size parameter of the dot in the plot
+            plt.scatter(df_dict[stock_name]['avg_daily_return'],df_dict[stock_name]['daily_risk'],s=30)
+
+            # loss calculation, 0.1 gives 90% accuracy
+            # Each number signifies that the total loss for a single day will not exceed this value
+            investment = 100000
+            loss = (abs(df_dict[stock_name]['avg_daily_return'].quantile(0.1))) * investment
+            print(stock_name,"max loss can be ",loss)
+
+
+        plt.title('Avg Daily Returns vs Risk of Stocks')
+        plt.legend(df_dict.keys())
+        plt.savefig('RiskvsReturnofStocks')
+
+    def future_stock_behavior(self):
+        csv_files=glob.glob("*.csv")
+
+
 
 
 
@@ -136,5 +245,8 @@ if __name__ == "__main__":
     obj=StockAnalysis()
     obj.closing_prices()
     obj.close_vs_moving_average()
-    obj.daily_returns()
-    obj.correlation()
+    obj.daily_returns_combined()
+    obj.daily_returns_individual()
+    obj.correlation_close()
+    obj.correlation_daily_returns()
+    obj.risk_estimation()
